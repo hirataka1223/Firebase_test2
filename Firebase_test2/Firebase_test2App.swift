7eb33756a5a6dbbfabac9a1659df0d18fa6f45a4
hirataka1223 <154341912+hirataka1223@users.noreply.github.com>
2024-01-30 21:09:48 +0900

    Initial Commit

---
+++ Firebase_test2/Firebase_test2App.swift
@@ -0,0 +1,17 @@
+//
+//  Firebase_test2App.swift
+//  Firebase_test2
+//
+//  Created by Taka on 2024/01/30.
+//
+
+import SwiftUI
+
+@main
+struct Firebase_test2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
